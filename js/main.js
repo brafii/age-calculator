880239efc3d2f508c4f18a6388620c578d7c7b20
Fiifi Ofori <fiifi56@gmail.com>
2022-05-21 17:28:39 +0000

    calculating date of birth with year

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 let calulateAge = document.getElementById("calAge");
 let dob = document.getElementById("dateOfBirth");
 
+const year = 2022;
+
 calulateAge.addEventListener("click", function(){
 
     let getInput = dob.value;
