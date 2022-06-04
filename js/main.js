ca94c9f02b99feeece0344fd29504c6e4393167d
Fiifi Ofori <fiifi56@gmail.com>
2022-06-04 19:32:15 +0000

    adding logic to user selecting age

--- js/main.js
+++ js/main.js
@@ -1,18 +1,57 @@
 let calulateAge = document.getElementById("calAge");
-let dob = document.getElementById("dateOfBirth");
+let userInput = document.getElementById("dateOfBirth").value;
+let dob = new Date(userInput);
 let showResult = document.getElementById("showAge");
 
-const year = 2022;
 
 calulateAge.addEventListener("click", function(){
 
-    let getInput = dob.value;
-    dob.value = "";
+    // let getInput = dob.value;
+    // dob.value = "";
 
-    let answer = year - getInput;
+    // let answer = year - getInput;
 
-    showResult.value = "You are " + answer + " years";
+    // showResult.value = "You are " + answer + " years";
 
-    console.log(answer);
+    // console.log(answer);
+
+    if(userInput === null || userInput === "")
+    {
+        showResult.value = "Please select a date";
+        return false;
+    }
+    else{
+
+        let mdate = userInput.toString();  
+        let dobYear = parseInt(mdate.substring(0,4), 10);  
+        let dobMonth = parseInt(mdate.substring(5,7), 10);  
+        let dobDate = parseInt(mdate.substring(8,10), 10); 
+
+         //get the current date from system  
+        let today = new Date();  
+        //date string after broking  
+        let birthday = new Date(dobYear, dobMonth-1, dobDate);  
+        
+        //calculate the difference of dates  
+        let diffInMillisecond = today.valueOf() - birthday.valueOf();  
+    
+        //convert the difference in milliseconds and store in day and year variable          
+        let year_age = Math.floor(diffInMillisecond / 31536000000);  
+        let day_age = Math.floor((diffInMillisecond % 31536000000) / 86400000);
+
+        //when birth date and month is same as today's date        
+        if ((today.getMonth() == birthday.getMonth()) && (today.getDate() == birthday.getDate())) {  
+            alert("Happy Birthday!");  
+        }  
+        
+        let month_age = Math.floor(day_age/30);          
+        day_ageday_age = day_age % 30;  
+            
+        let tMnt= (month_age + (year_age*12));  
+        let tDays =(tMnt*30) + day_age;
+        
+    }
+
+    
 
 });
