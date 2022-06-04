ef312720c8f3afc1aad5af95d41b016fc80eebee
Fiifi Ofori <fiifi56@gmail.com>
2022-06-04 20:00:06 +0000

    testing user input with date

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let calulateAge = document.getElementById("calAge");
-let userInput = document.getElementById("dateOfBirth").value;
-let dob = new Date(userInput);
+// let userInput = document.getElementById("dateOfBirth").value;
+// let dob = new Date(userInput);
 let showResult = document.getElementById("showAge");
 
 
@@ -15,42 +15,55 @@ calulateAge.addEventListener("click", function(){
 
     // console.log(answer);
 
-    if(userInput === null || userInput === "")
-    {
-        showResult.value = "Please select a date";
-        return false;
-    }
-    else{
-
-        let mdate = userInput.toString();  
-        let dobYear = parseInt(mdate.substring(0,4), 10);  
-        let dobMonth = parseInt(mdate.substring(5,7), 10);  
-        let dobDate = parseInt(mdate.substring(8,10), 10); 
-
-         //get the current date from system  
-        let today = new Date();  
-        //date string after broking  
-        let birthday = new Date(dobYear, dobMonth-1, dobDate);  
-        
-        //calculate the difference of dates  
-        let diffInMillisecond = today.valueOf() - birthday.valueOf();  
+    //collect input from HTML form and convert into date format
+    var userinput = document.getElementById("dateOfBirth").value;
+    var dob = new Date(userinput);
     
-        //convert the difference in milliseconds and store in day and year variable          
-        let year_age = Math.floor(diffInMillisecond / 31536000000);  
-        let day_age = Math.floor((diffInMillisecond % 31536000000) / 86400000);
+    //check user provide input or not
+    if(userinput==null || userinput==''){
+      document.getElementById("message").innerHTML = "**Choose a date please!";  
+      return false; 
+    } 
+    
+    //execute if user entered a date 
+    else {
+    //extract and collect only date from date-time string
+    var mdate = userinput.toString();
+    var dobYear = parseInt(mdate.substring(0,4), 10);
+    var dobMonth = parseInt(mdate.substring(5,7), 10);
+    var dobDate = parseInt(mdate.substring(8,10), 10);
+    
+    //get the current date from system
+    var today = new Date();
+    //date string after broking
+    var birthday = new Date(dobYear, dobMonth-1, dobDate);
+    
+    //calculate the difference of dates
+    var diffInMillisecond = today.valueOf() - birthday.valueOf();
 
-        //when birth date and month is same as today's date        
-        if ((today.getMonth() == birthday.getMonth()) && (today.getDate() == birthday.getDate())) {  
-            alert("Happy Birthday!");  
-        }  
+    //convert the difference in milliseconds and store in day and year variable        
+    var year_age = Math.floor(diffInMillisecond / 31536000000);
+    var day_age = Math.floor((diffInMillisecond % 31536000000) / 86400000);
+
+    //when birth date and month is same as today's date      
+    if ((today.getMonth() == birthday.getMonth()) && (today.getDate() == birthday.getDate())) {
+            alert("Happy Birthday!");
+        }
         
-        let month_age = Math.floor(day_age/30);          
-        day_ageday_age = day_age % 30;  
-            
-        let tMnt= (month_age + (year_age*12));  
-        let tDays =(tMnt*30) + day_age;
+     var month_age = Math.floor(day_age/30);        
+     day_age = day_age % 30;
         
-    }
+     var tMnt= (month_age + (year_age*12));
+     var tDays =(tMnt*30) + day_age;
+     
+    //DOB is greater than today?s date, generate an error: Invalid date  
+     if (dob>today) {
+        document.getElementById("result").innerHTML = ("Invalid date input - Please try again!");
+      }
+      else {
+        document.getElementById("result").innerHTML = year_age + " years " + month_age + " months " + day_age + " days"
+      }
+   }
 
     
 
